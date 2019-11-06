45321d908885630ebb4bcafffb6d1e6c727cc881
Griffin Roth <griffin.roth@tcs.com>
2019-11-06 13:45:56 -0500

    login view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Welcome">
+        Welcome
       </header>
+      <p className="Description">
+      This is the login page for the Discord App Botify, which lets you keep track of all your favorite artists.
+      </p>
+      <button className="SpotButton"
+      onClick="">
+        Sign Up With Spotify
+        </button>
     </div>
   );
 }
