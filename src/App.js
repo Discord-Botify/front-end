9643d627124736cbd7173000d520a6a24f86e9ae
Michael Rotuno <rotunomp@miamioh.edu>
2019-11-08 12:36:53 -0500

    Set up Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="Welcome">
-        Welcome
-      </header>
-      <p className="Description">
-      This is the login page for the Discord App Botify, which lets you keep track of all your favorite artists.
-      </p>
-      <button className="SpotButton"
-      onClick="">
-        Sign Up With Spotify
-        </button>
-    </div>
+      <Router>
+
+          <Route exact path={'/'} render={props => (
+              <React.Fragment>
+                  <div className="App">
+                      <header className="Welcome">
+                          Welcome
+                      </header>
+                      <p className="Description">
+                          This is the login page for the Discord App Botify, which lets you keep track of all your favorite artists.
+                      </p>
+                      <button className="SpotButton"
+                              onClick="">
+                          Sign Up With Spotify
+                      </button>
+                  </div>
+              </React.Fragment>
+          )}/>
+
+      </Router>
+
   );
 }
 
