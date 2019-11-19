e835a8eeb8a48d80c29685375382c3ecdb52494c
Michael Rotuno <rotunomp@miamioh.edu>
2019-11-19 11:17:18 -0500

    updated discord oauth route

--- src/reRoute/DiscordAuthRedirect.js
+++ src/reRoute/DiscordAuthRedirect.js
@@ -5,7 +5,7 @@ import qs from 'query-string';
 
 class DiscordAuthRedirect extends Component {
 
-    apiToken = 'http://localhost:4567/oauth';
+    apiToken = 'http://localhost:4567/oauth/discord';
     state = {
         queryString: '',
     };
