30d8cf9093c45ebe1285b5d5f25d73f480506793
rotunomp <rotunomp@miamioh.edu>
2019-11-25 16:27:36 +0000

    working cookies in redirect

--- src/reRoute/DiscordAuthRedirect.js
+++ src/reRoute/DiscordAuthRedirect.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom'
+import {withRouter} from 'react-router'
 import Axios from "axios";
 import qs from 'query-string';
 import SessionCookie from '../components/SessionCookie'
@@ -9,12 +10,21 @@ class DiscordAuthRedirect extends Component {
     apiToken = 'https://api.michaelrotuno.dev:4567/oauth/discord';
     state = {
         queryString: '',
+	stoken: 'notoken',
+	redirect: false,
     };
 
     constructor(props) {
         super(props);
         this.state.queryString = window.location.search.substring(1);
     }
+
+   componentDidUpdate(){
+	if(this.state.redirect){
+		document.cookie="stoken="+this.state.stoken;
+		this.props.history.push("/home");
+	}
+   }
 
     // Get the auth token from our backend
     authAndRedirect = (code) => {
@@ -26,8 +36,9 @@ class DiscordAuthRedirect extends Component {
 
         Axios.post(this.apiToken, {'code': code}, {headers: headers})
             .then(response => {
-                console.log(response);
-                SessionCookie("response.data.appSessionId");
+                console.log(response.data.appSessionId);
+                this.setState({stoken: response.data.appSessionId, redirect: true});
+		console.log(this.state.stoken);
             });
 
         // Set all the cookies
@@ -41,11 +52,10 @@ class DiscordAuthRedirect extends Component {
         return (
             <div>
                 {this.authAndRedirect(
-                    qs.parse(this.state.queryString, { ignoreQueryPrefix: true }).code
-                )}
+                    qs.parse(this.state.queryString, { ignoreQueryPrefix: true }).code)}
             </div>
         );
     }
 }
 
-export default DiscordAuthRedirect;
+export default withRouter(DiscordAuthRedirect);
